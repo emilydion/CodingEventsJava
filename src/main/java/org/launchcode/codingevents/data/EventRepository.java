0554e312ce2b302b7ffde2427afb233f341ddb1d
emilydion <edion468@gmail.com>
2024-05-28 22:32:27 -0500

    updated with persistence

--- src/main/java/org/launchcode/codingevents/data/EventRepository.java
+++ src/main/java/org/launchcode/codingevents/data/EventRepository.java
@@ -7,3 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface EventRepository extends CrudRepository<Event, Integer> {
 }
+
+// CrudRepository creates public class blank extends EventRepository on the fly to give the "concrete" to the "contract" of the interface
